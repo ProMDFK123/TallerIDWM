5f6fbbd826ecf3eea7753b54f9bfeee883105300
yakusu123 <vic3nt3.ord3n3s@gmail.com>
2025-04-18 18:21:36 -0400

    Feat(models): add user, products and addres to project

---
+++ TallerIDWM-main/src/Data/DbInitializer.cs
@@ -0,0 +1,54 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using api.src.Data;
+using Ayudantia.src.models;
+
+using Bogus;
+
+using Microsoft.EntityFrameworkCore;
+
+namespace api.src.Data
+{
+    public class DbInitializer
+    {
+        public static void InitDb(WebApplication app)
+        {
+            using var scope = app.Services.CreateScope();
+
+            var context = scope.ServiceProvider.GetRequiredService<DataContext>()
+                ?? throw new InvalidOperationException("Could not get DataContext");
+
+            SeedData(context);
+        }
+
+        private static void SeedData(DataContext context)
+        {
+            context.Database.Migrate();
+
+            if (context.Products.Any()) return;
+
+            var faker = new Faker("es");
+
+            var products = new Faker<Product>()
+                .RuleFor(p => p.Name, f => f.Commerce.ProductName())
+                .RuleFor(p => p.Category, f => f.Commerce.Department())
+                .RuleFor(p => p.Description, f => f.Commerce.ProductDescription())
+                .RuleFor(p => p.Price, f => f.Random.Decimal(5000,50000))
+                .RuleFor(p => p.Brand, f => f.Company.CompanyName())
+                .RuleFor(p => p.Stock, f => f.Random.Int(10,200))
+                .RuleFor(p => p.Urls, f => new[]
+                {
+                    $"https://res.cloudinary.com/demo/image/upload/sample1.jpg",
+                    $"https://res.cloudinary.com/demo/image/upload/sample2.jpg",
+                    $"https://res.cloudinary.com/demo/image/upload/sample3.jpg"
+                })
+                .Generate(10);
+
+            context.Set<Product>().AddRange(products);
+            context.SaveChanges();
+
+        }
+    }
+}
