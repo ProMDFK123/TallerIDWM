ce1266febeab00d5d818d5daefc2b2d7c692c435
Gabriel Lopez <89820038+ProMDFK123@users.noreply.github.com>
2025-04-19 09:56:30 -0400

    Revert "feat(controller): product controller added"
    This reverts commit 232ed5e6cc5fe4b55c3a9975db51c820565911f1.

---
+++ TallerIDWM-main/src/Data/DataContext.cs
@@ -0,0 +1,9 @@
+using api.src.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace api.src.Data{
+    public class ClonedDataContext(DbContextOptions<DataContext> options) : DbContext(options){
+        public DbSet<Store> Stores {get; set;} 
+        public DbSet<Product> Products {get; set;}
+    }
+}
