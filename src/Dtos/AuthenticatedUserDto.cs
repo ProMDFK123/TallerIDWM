774ef6c68587d5bd14702ac2853f5e31a399931f
Gabriel Lopez <89820038+ProMDFK123@users.noreply.github.com>
2025-05-09 16:25:10 -0400

    feat: authenticated user information

--- src/Dtos/AuthenticatedUserDto.cs
+++ src/Dtos/AuthenticatedUserDto.cs
@@ -7,5 +7,12 @@ namespace api.src.Dtos
 {
     public class AuthenticatedUserDto
     {
+        public int Id { get; set; }
+        public string UserName { get; set; }
+        public string Email { get; set; }
+        public string Token { get; set; }
+        public string Role { get; set; }
+        public DateTime RegisteredAt { get; set; } = DateTime.UtcNow;
+        public bool IsActive { get; set; } = true;
     }
 }
