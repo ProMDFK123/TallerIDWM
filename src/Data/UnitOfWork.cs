b50c081169c119b19f6a2c2490095a8b8e28666b
Gabriel Lopez <89820038+ProMDFK123@users.noreply.github.com>
2025-05-08 20:23:08 -0400

    feat(data): unit of work added

--- src/Data/UnitOfWork.cs
+++ src/Data/UnitOfWork.cs
@@ -2,12 +2,19 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using api.src.Interfaces;
 using TallerIDWM.src.Data;
 
-namespace TallerIDWM.src.Data
+namespace TallerIDWM.src.Data;
+public class UnitOfWork(DataContext context, IProductRepository productRepository, IAddress1Repository address1Repository, IUserRepository userRepository)
 {
-    public class UnitOfWork
+    private readonly DataContext dataContext = context;
+    public IProductRepository ProductRepository { get; } = productRepository;
+    public IAddress1Repository Address1Repository { get; } = address1Repository;
+    public IUserRepository UserRepository { get; } = userRepository;
+
+    public async Task SaveChangesAsync()
     {
-        
+        await dataContext.SaveChangesAsync();
     }
 }
