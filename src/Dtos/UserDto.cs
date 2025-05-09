be0baf5369a4ac11e7728596023bf39c5c6a3ae1
Gabriel Lopez <89820038+ProMDFK123@users.noreply.github.com>
2025-05-09 16:39:25 -0400

    feat: userdto information

--- src/Dtos/UserDto.cs
+++ src/Dtos/UserDto.cs
@@ -16,6 +16,10 @@ namespace TallerIDWM.src.Dtos
         public required string? commune { get; set; }
         public required string? Region { get; set; }
         public required string? postalCode { get; set; }
-
+        public required string? Id { get; set; }
+        public required DateTime RegisteredAt { get; set; } = DateTime.UtcNow;
+        public required bool IsActive { get; set; } = true;
+        public required string? UserName { get; set; }
+        public required string? Password { get; set; }
     }
 }
