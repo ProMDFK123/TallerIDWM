308e45b93d71a61dad1b0fa44956f958a55d5e6b
Gabriel Lopez <89820038+ProMDFK123@users.noreply.github.com>
2025-05-09 16:37:45 -0400

    fix: syntax error

--- src/Controllers/AuthController.cs
+++ src/Controllers/AuthController.cs
@@ -13,7 +13,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http.HttpResults;
-using Microsoft.AspNetCore.Mvc;
 
 namespace api.src.Controllers
 {
