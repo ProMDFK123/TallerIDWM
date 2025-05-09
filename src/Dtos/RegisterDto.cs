f5a967c06126fca0e2682a1793eedab0a6c5da7e
Gabriel Lopez <89820038+ProMDFK123@users.noreply.github.com>
2025-05-09 16:02:34 -0400

    feat: RegisterDto information

--- src/Dtos/RegisterDto.cs
+++ src/Dtos/RegisterDto.cs
@@ -7,9 +7,18 @@ namespace api.src.Dtos
 {
     public class RegisterDto
     {
-        public string? Username { get; set; }
+        public string? UserName { get; set; }
         public string? Email { get; set; }
         public string? Password { get; set; }
         public string? ConfirmPassword { get; set; }
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public string? PhoneNumber { get; set; }
+        public string? Address { get; set; }
+        public string? City { get; set; }
+        public string? State { get; set; }
+        public string? Country { get; set; }
+        public string? ZipCode { get; set; }
+        public string? Role { get; set; }
     }
 }
