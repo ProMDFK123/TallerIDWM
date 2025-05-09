d4cba1de48920f0cb099340d9b193c4e83499110
Gabriel Lopez <89820038+ProMDFK123@users.noreply.github.com>
2025-05-09 16:53:44 -0400

    fix: usermapper errors

--- src/Dtos/AuthenticatedUserDto.cs
+++ src/Dtos/AuthenticatedUserDto.cs
@@ -7,7 +7,7 @@ namespace api.src.Dtos
 {
     public class AuthenticatedUserDto
     {
-        public int Id { get; set; }
+        public string Id { get; set; }
         public string UserName { get; set; }
         public string Email { get; set; }
         public string Token { get; set; }
