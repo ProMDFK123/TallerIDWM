382c814589d4ba4e7d5c3dbdd8537b50ea83eaec
Gabriel Lopez <89820038+ProMDFK123@users.noreply.github.com>
2025-05-15 18:35:28 -0400

    fix: syntax error

--- src/Dtos/User/ChangePassswordDto.cs
+++ src/Dtos/User/ChangePassswordDto.cs
@@ -1,11 +1,12 @@
 using System.ComponentModel.DataAnnotations;
 
+
 namespace TallerIDWM.Src.DTOs.User
 {
     public class ChangePasswordDto
     {
         [Required(ErrorMessage = "La contraseña actual es obligatoria.")]
-        [StringLength(100, MinimumLenght = 8, ErrorMessage = "La contraseña actual debe tener al menos 8 caracteres.")]
+        [StringLength(100, MinimumLength = 8, ErrorMessage = "La contraseña actual debe tener al menos 8 caracteres.")]
         [RegularExpression(
             @"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$",
             ErrorMessage = "La contraseña actual debe contener al menos una letra mayúscula, una letra minúscula, un número y un carácter especial."
@@ -13,7 +14,7 @@ namespace TallerIDWM.Src.DTOs.User
         public string CurrentPassword { get; set; } = string.Empty;
 
         [Required(ErrorMessage = "La nueva contraseña es obligatoria.")]
-        [StringLength(100, MinimumLenght = 8, ErrorMessage = "La nueva contraseña debe tener al menos 8 caracteres.")]
+        [StringLength(100, MinimumLength = 8, ErrorMessage = "La nueva contraseña debe tener al menos 8 caracteres.")]
         [RegularExpression(
             @"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$",
             ErrorMessage = "La nueva contraseña debe contener al menos una letra mayúscula, una letra minúscula, un número y un carácter especial."
